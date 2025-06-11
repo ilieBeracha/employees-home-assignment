bc022526cb6529b81909b57c1c231ee0b9a6faf9
ilie-ber <iliea@modelyo.com>
2025-06-11 20:10:41 +0300

    added dialog

--- src/app/edit-employee-dialog/edit-employee-dialog.component.ts
+++ src/app/edit-employee-dialog/edit-employee-dialog.component.ts
@@ -1,43 +1,42 @@
 import { Component, EventEmitter, Inject, inject, Output } from '@angular/core';
 import { Employee } from '../../models/employee';
 import { employeesStore } from '../../stores/employees-store';
-import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
-import { NgFor } from '@angular/common';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { CommonModule, NgFor } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { EmployeesViewComponent } from '../employees-view/employees-view.component';
+import { FormsModule, NgModel } from '@angular/forms';
+
 
 @Component({
   selector: 'app-edit-employee-dialog',
-  imports: [NgFor, MatDialogModule],
-  providers: [employeesStore],
+  imports: [CommonModule, FormsModule, MatDialogModule, MatFormFieldModule, MatSelectModule, MatInputModule, MatButtonModule, MatIconModule],
+  providers: [employeesStore  ],
   templateUrl: './edit-employee-dialog.component.html',
+  standalone: true,
   styleUrl: './edit-employee-dialog.component.scss'
 })
 export class EditEmployeeDialogComponent {
-
-  employeesStore = inject(employeesStore);
-  @Output() newEmployee = new EventEmitter<Employee>();
-
-  constructor(
-    public dialogRef: MatDialogRef<EditEmployeeDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: Employee
-  ) {
+  readonly employeesStore = inject(employeesStore);
+  constructor(public dialogRef: MatDialogRef<EmployeesViewComponent>, @Inject(MAT_DIALOG_DATA) public data: Employee)  { 
     this.employeesStore.loadEmployees();
-
   }
+  @Output() PostEditeEmployeeResponse = new EventEmitter<Employee>()
 
-  departments = this.employeesStore.departments();
+  saveEmployee() {
+    this.PostEditeEmployeeResponse.emit(this.data);
+    this.dialogRef.close(this.data);
+  }
+  employees = this.employeesStore.employees();
+  departments = this.employeesStore.departments(); 
   cities = this.employeesStore.cities();
 
   closeDialog() {
     this.dialogRef.close();
   }
 
-  saveEmployee() {
-    this.newEmployee.emit(this.data);
-    this.dialogRef.close(this.data);
-  }
-
-  onSubmit() {
-    this.newEmployee.emit(this.data);
-    this.dialogRef.close(this.data);
-  }
 } `      `
